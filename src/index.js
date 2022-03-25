31a831c92d179111ab720a34efa1c6525d538d3b
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2022-03-25 15:44:36 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const axios = require("axios");
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const huddleAPI = process.env.API;
+const huddleKey = process.env.API_KEY;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("Hi mom");
+});
+
+app.post("/api/getHuddle/:contract", (req, res) => {
+	axios
+		.post(
+			huddleAPI,
+			{
+				title: "Metapass Event",
+				contractAd1: req.params.contract,
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${huddleKey}`,
+				},
+			}
+		)
+		.then((res) => console.log(res.data?.meetingLink));
+	console.log(req.params.contract);
+	res.sendStatus(200);
+});
+
+app.listen(PORT, () => {
+	console.log("Server is running on port ", PORT);
+});
+
+module.exports = app;
