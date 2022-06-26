29dd62f3f5eabe5eb9a264fc82cd63199031caf7
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2022-06-26 20:43:58 +0530

    feat: add post endpoint

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const axios = require("axios");
 require("dotenv").config();
+const cors = require("cors")
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 const huddleAPI = process.env.API;
 const huddleKey = process.env.API_KEY;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
 	res.send("Hi mom");
@@ -34,11 +36,39 @@ app.get("/api/getHuddle/:contract", (req, res) => {
 		)
 		.then((el) => {
 			console.log(el.data?.roomId);
+			res.setHeader("Access-Control-Allow-Origin", "http://localhost:8000")
+			res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+			res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
 			res.send({
 				roomId: el.data?.roomId,
 			});
 		});
 });
+
+app.post("/", (req, res) => {
+	const { contractAddress, host } = req.body;
+	axios
+		.post(
+			huddleAPI,
+			{
+				title: "Metapass Event",
+				contractAd1: contractAddress,
+				chain: "polygon",
+				host: host,
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${huddleKey}`,
+				},
+			}
+		)
+		.then((el) => {
+			console.log(el.data?.meetingLink);
+			res.send({
+				meetingLink: el.data?.meetingLink,
+			});
+		});
+})
 
 app.listen(PORT, () => {
 	console.log("Server is running on port ", PORT);
